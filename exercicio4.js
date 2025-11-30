9dfcbf5caa740e585d251c2218273512d9586157
Paulo Henrique de Barros Brandão <paulobrandaohbb@gmail.com>
2025-11-30 13:36:08 -0300

    subindo exercicio4.js

---
+++ exercicio4.js
@@ -0,0 +1,6 @@
+const numeros = require("readline-sync");
+let n1 = numeros.questionInt("digite um valor:");
+let n2 = numeros.questionInt("digite um valor:");
+let n3 = numeros.questionInt("digite um valor:");
+let n4 = numeros.questionInt("digite um valor:");
+console.log("A diferença do produto é:", (n1 * n2) - (n3 * n4));
